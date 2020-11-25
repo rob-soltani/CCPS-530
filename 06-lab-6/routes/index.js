ef37d594a934d8da2e78b8ef48c1659ea1e8d63a
Rob Soltani @ Rob-Dell <rob@soltani.io>
2020-11-24 21:41:04 -0500

    Lab6: Express Generator added with placeholder NodeJS files for the API points

---
+++ 06-lab-6/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CCPS 530 - Lab 6' });
+});
+
+module.exports = router;
