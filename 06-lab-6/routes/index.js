d2ca5cc3ba0260051d2582b8e57c56b86d172d0a
Rob Soltani @ Rob-Dell <rob@soltani.io>
2020-11-24 23:50:18 -0500

    CCPS 530 Lab 6 Finished

--- 06-lab-6/routes/index.js
+++ 06-lab-6/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CCPS 530 - Lab 6' });
+  res.render('index', { title: 'CCPS 530 - Fall 2020 - Lab 6 - Express and NodeJS' });
 });
 
 module.exports = router;
