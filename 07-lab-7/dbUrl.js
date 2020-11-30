66ff6c9a33875488c1625cf39869398a3949afd4
Rob Soltani @ Rob-Dell <rob@soltani.io>
2020-11-30 13:33:07 -0500

    Update Lab 7 to work with Heroku

--- 07-lab-7/dbUrl.js
+++ 07-lab-7/dbUrl.js
@@ -1,4 +1,3 @@
-// const MongoDB_URL = "mongodb://localhost:27017/";
-const MongoDB_URL = "mongodb+srv://root:<password>@cluster0.lzme8.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const MongoDB_URL = process.env.MONGODB_URI || "mongodb://localhost:27017/";
 
 module.exports = MongoDB_URL;
